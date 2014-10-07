84c4199774a9c0a553edbf837a722f2897829ecc
Mauricio <mpayetta@gmail.com>
2014-10-07 20:57:14 +0200

    added function to position the cursor at the end of an element

--- writer.js
+++ writer.js
@@ -84,6 +84,7 @@ var Writer = function (p_elem, p_options) {
         placeholder.textContent = writerOptions.placeholder;
         writercont.appendChild(placeholder);
         placeholder.focus();
+        placeCursorAtEnd(placeholder);
     }
      
     /*
@@ -106,6 +107,28 @@ var Writer = function (p_elem, p_options) {
         }
          
     }
+    
+    /*
+     * Places the cursor at the end of the given element
+     */
+    function placeCursorAtEnd(el) {
+        el.focus();
+        if (typeof window.getSelection != "undefined"
+                && typeof document.createRange != "undefined") {
+            var range = document.createRange();
+            range.selectNodeContents(el);
+            range.collapse(false);
+            var sel = window.getSelection();
+            sel.removeAllRanges();
+            sel.addRange(range);
+        } else if (typeof document.body.createTextRange != "undefined") {
+            var textRange = document.body.createTextRange();
+            textRange.moveToElementText(el);
+            textRange.collapse(false);
+            textRange.select();
+        }
+    }
+
      
     /*
      * Returns the first parent element of the given element that is a block
